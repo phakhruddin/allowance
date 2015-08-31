2e3e9327c8832af86a58ad50b2170ac16e5b355c
Abdullah <dabdullah@SDGL13dae2506.local>
2015-08-30 21:06:45 -0500

    add icon

--- app/controllers/debit.js
+++ app/controllers/debit.js
@@ -7,10 +7,71 @@ function addHandler(e) {
 	console.log("JSON stringify addHandler(e): "+JSON.stringify(e));
 }
 
- function myRefresher(e) {
-	console.log("refreshing after pull : " +JSON.stringify(e));
-    Alloy.Collections.debit.fetch({
-        success: e.hide,
-        error: e.hide
-    });
-}
+function debitDetailAddRow (date,dateadded) {
+		console.log("enterpayment.js::debitDetailAddRow: date: "+date+"  dateadded: "+dateadded+" new Date(+dateadded): "+new Date(+dateadded));
+	    var debitrow = Ti.UI.createTableViewRow ({
+                backgroundColor: "white",
+                opacity:"0",
+                color:"transparent",
+                width: Ti.UI.SIZE,
+                height: Ti.UI.SIZE
+        });
+	
+        var datelabel = Ti.UI.createLabel ({
+                color : "orange",
+                font : {
+                	fontSize : 10
+                },
+                left  : "20",
+                textAlign : "Ti.UI.TEXT_ALIGNMENT_LEFT",
+                top : "10",
+                text : dateadded
+        });
+        var datespend = Ti.UI.createLabel ({
+                color : "#333",
+                font : {
+                	fontSize : 24
+                },
+                left  : "20",
+                textAlign : "Ti.UI.TEXT_ALIGNMENT_LEFT",
+                top : "50",
+                text : date.toLocaleString()
+                //text : date.toString().split(' ')[1]+" "+date.toString().split(' ')[2]+" "+date.toString().split(' ')[3]
+        });
+        var blueline = Ti.UI.createImageView ({
+                left  : "20",
+                textAlign : "Ti.UI.TEXT_ALIGNMENT_LEFT",
+                top : "30",
+                width : "85%",
+                height : "3",
+                image : "blueline.png"
+        });
+        var innerview = Ti.UI.createView({
+                width:"30%",
+                height:"Ti.UI.Size",
+                left:"60%",
+                top:"40",
+                backgroundColor:"white",
+                borderRadius:"10",
+                borderWidth:"0.1",
+                borderColor:"white"
+        });
+        debitrow.add(datelabel);
+        debitrow.add(datespend);
+        debitrow.add(blueline);
+        debitrow.add(innerview);
+        debitrow.add(datelabel);
+        debitrow.add(blueline);
+        debitrow.metadata = dateadded; // add metadata info
+        
+        var debittable = Ti.UI.createTableView({
+                backgroundColor: "white",
+                separatorStyle :"Titanium.UI.iPhone.TableViewSeparatorStyle.NONE"
+        });
+        debittable.add(debitrow);
+        
+        $.debit_table.appendRow(debitrow);
+
+};
+
+debitDetailAddRow("1/1/2008","2/1/2015");
