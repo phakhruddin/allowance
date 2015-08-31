de15eaab197a7500138b8ef9e20f15a94f753d1a
Abdullah <dabdullah@SDGL13dae2506.local>
2015-08-31 14:35:25 -0700

    add transaction

--- app/alloy.js
+++ app/alloy.js
@@ -9,3 +9,15 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
+
+//AMPM
+Alloy.Globals.formatAMPM = function(date){
+	var hours = date.getHours();
+  	var minutes = date.getMinutes();
+	  var ampm = hours >= 12 ? 'pm' : 'am';
+	  hours = hours % 12;
+	  hours = hours ? hours : 12; // the hour '0' should be '12'
+	  minutes = minutes < 10 ? '0'+minutes : minutes;
+	  var strTime = hours + ':' + minutes + ' ' + ampm;
+	  return strTime;
+};
--- app/controllers/transaction.js
+++ app/controllers/transaction.js
@@ -1,27 +1,6 @@
-exports.openMainWindow = function(_tab) {
-  _tab.open($.debit_window);
-  Ti.API.info("This is child widow schedule.js" +JSON.stringify(_tab));
-  
-  $.catLabel.addEventListener('click',function(e){
-  console.log("JSON stringify catLabel(e): "+JSON.stringify(e));
-  $.input_view.add(picker);
-  $.input_view.remove($.date_picker);
-});
-
-};
-
-function addHandler(e) {
-	console.log("JSON stringify addHandler(e): "+JSON.stringify(e));
-}
-
-function setDate(e){
-	console.log("JSON stringify setDate(e): "+JSON.stringify(e));
-	var date = e.value;
-	$.dateLabel.text= (date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear()+" "+Alloy.Globals.formatAMPM(date);
-}
-
+console.log("transaction.js")
 function debitDetailAddRow (date,dateadded,category,amount) {
-		console.log("enterpayment.js::debitDetailAddRow: date: "+date+"  dateadded: "+dateadded+" new Date(+dateadded): "+new Date(+dateadded));
+		console.log("transaction.js::debitDetailAddRow: date: "+date+"  dateadded: "+dateadded);
 	    var debitrow = Ti.UI.createTableViewRow ({
                 backgroundColor: "white",
                 opacity:"0",
@@ -104,8 +83,7 @@ function debitDetailAddRow (date,dateadded,category,amount) {
         });
         debittable.add(debitrow);
         
-        $.debit_table.appendRow(debitrow);
-
+      $.transaction_table.appendRow(debitrow);
 };
 
 debitDetailAddRow("8/22/2015","8/1/2015","Grocery","$200.00");
@@ -113,51 +91,3 @@ debitDetailAddRow("8/23/2015","8/2/2015","Book","$205.00");
 debitDetailAddRow("8/24/2015","8/3/2015","Book","$301.00");
 debitDetailAddRow("8/25/2015","8/4/2015","Book","$221.00");
 debitDetailAddRow("8/26/2015","8/5/2015","Book","$132.00");
-
-var picker = Ti.UI.createPicker({
-  top:90
-  
-});
-var data = []; var cat = [];
-var cat = ['Book', 'Grocery', 'Transport','Fee', 'Stationary', 'Electronics', 'Travel'];
-for (i=0;i<cat.length;i++){
-	data =  Ti.UI.createPickerRow({title:cat[i],font:{ fontSize:24 }});
-	picker.add(data);
-}
-picker.selectionIndicator = true;
-
-$.catLabel.color="red";
-picker.addEventListener('change',function(e){
-  console.log("JSON stringify picker(e): "+JSON.stringify(e));
-  $.catLabel.text=e.row.title;
-  $.catLabel.color="blue";
-  //$.input_view.remove(picker);
-});
-
-$.input_view.remove($.date_picker);
-$.input_view.add(picker);
-
-
-
-$.costLabel.addEventListener('click',function(e){
-  console.log("JSON stringify costLabel(e): "+JSON.stringify(e));
-  $.input_view.remove(picker);
-  $.costTF.show();
-});
-
-function notesAreaFocus(e) {
-	console.log("JSON stringify notes_textarea(e): "+JSON.stringify(e));
-	$.notes_textarea.borderColor="#bbb";
-}
-
-function blurIT(e){
-	console.log("JSON stringify blurIT(e): "+JSON.stringify(e));
-	$.notes_textarea.blur();
-}
-
-$.dateLabel.addEventListener('click',function(e){
-  console.log("JSON stringify dateLabel(e): "+JSON.stringify(e));
-  $.input_view.add($.date_picker);
-  $.dateLabel.color="blue";
-});
-
