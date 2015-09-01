97125e786aea93ed26c266c4f39110c7d0fe74f1
Abdullah <dabdullah@SDGL13dae2506.lan>
2015-08-31 19:50:26 -0700

    add collections

--- app/alloy.js
+++ app/alloy.js
@@ -21,3 +21,62 @@ Alloy.Globals.formatAMPM = function(date){
 	  var strTime = hours + ':' + minutes + ' ' + ampm;
 	  return strTime;
 };
+
+console.log("Ti.Filesystem.tempDirectory: "+Ti.Filesystem.tempDirectory);
+Alloy.Globals.writeFile = function (content, filename){
+			var file = Ti.Filesystem.getFile(
+				Ti.Filesystem.tempDirectory, filename
+			);
+			file.write(content+"\n");
+};
+
+//download json data from published google spreadsheet
+Alloy.Globals.updatemodelTable = function(type,col1,col2,col3,col4,col5,col6,col7,col8,col9) {
+	console.log("Alloy.Globals.updatemodelTable: type: "+type+" :col1: "+col1);
+	var model = Alloy.createModel(type,{
+		col1 : col1,
+		col2 : col2,
+		col3 : col3,
+		col4 : col4,
+		col5 : col5,
+		col6 : col6,
+		col7 : col7,
+		col8 : col8,
+		col9 : col9
+	});			
+	model.save();
+};
+
+//update local sqllite DB
+Alloy.Globals.updateType = function(url,type) {
+	console.log("Alloy.Globals.updateType: type: "+type+" :url: "+url);
+	var xhr = Ti.Network.createHTTPClient({
+		    onload: function(ee) {
+		    		eval("Alloy.Collections."+type+".deleteAll()"); // cleanup data. overwrite existing.
+			    	json = JSON.parse(this.responseText);
+			    	console.log("Alloy.Globals.updateType : json: "+json);
+			    	var out = '{ "poi" : ['+"\n";
+			    	for (var i=0; i < json.feed.entry.length; i++) {
+			    		var col1 = json.feed.entry[i].title.$t.trim();
+						var col2= json.feed.entry[i].content.$t.split(',')[0].split(':')[1].trim() || "none";
+						var col3 = json.feed.entry[i].content.$t.split(',')[1].split(':')[1].trim() || "none";
+						var col4 = json.feed.entry[i].content.$t.split(',')[2].split(':')[1].trim() || "none";
+						var col5 = json.feed.entry[i].content.$t.split(',')[3].split(':')[1].trim() || "none";
+						var col6 = json.feed.entry[i].content.$t.split(',')[4].split(':')[1].trim() || "none";
+						var col7 = json.feed.entry[i].content.$t.split(',')[5].split(':')[1].trim() || "none";
+						var col8= json.feed.entry[i].content.$t.split(',')[6].split(':')[1].trim() || "none";
+						var col9=  json.feed.entry[i].content.$t.split(',')[7].split(':')[1].trim() || "none";
+						Alloy.Globals.updatemodelTable(type,col1,col2,col3,col4,col5,col6,col7,col8,col9);	
+						if ( i == (json.feed.entry.length - 1)) {
+							out += '{ "col1" : "'+col1+'" , "col2" : "'+col2+'" , "col3" : "'+col3+'"  , "col4" : "'+col4+'" , "col5" : "'+col5+'" , "col6" : "'+col6+'" , "col7" : "'+col7+'" , "col8" : "'+col8+'" , "col9" : "'+col9+'" }]}'+"\n";
+						} else {
+							out += '{ "col1" : "'+col1+'" , "col2" : "'+col2+'" , "col3" : "'+col3+'"  , "col4" : "'+col4+'" , "col5" : "'+col5+'" , "col6" : "'+col6+'" , "col7" : "'+col7+'" , "col8" : "'+col8+'" , "col9" : "'+col9+'" },'+"\n";
+						}
+			    	}
+					Alloy.Globals.writeFile(out,type+".json");
+					var json = out;		    
+			}	
+		});			
+		xhr.open("GET", url);
+		xhr.send();
+};
--- app/controllers/main.js
+++ app/controllers/main.js
@@ -9,3 +9,9 @@ $.lastdebit_button.addEventListener ("click", function(e){
 	var tabViewOneController = Alloy.createController("debit");
 	tabViewOneController.openMainWindow($.main_tab);	
 });
+
+function debitAction(e){
+	var url="https://spreadsheets.google.com/feeds/list/11zxiijjENT69g_97R8nvLZvv_hfBC1tdsJrJ6skNBVE/od6/public/basic?hl=en_US&alt=json"
+	var type="debitmodel";
+	Alloy.Globals.updateType(url,type);
+}
